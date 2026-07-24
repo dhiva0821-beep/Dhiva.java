b5f0fdd35d933e12629b1899ce50c48b2a879bb0
Dhiva.java <dhiva0821@gmail.com>
2026-07-24 09:42:57 +0530

    day 24

--- Array.java
+++ Array.java
@@ -1,8 +1,9 @@
 public class Array {
     public static void main(Stringex[] args) {
-        int [] arr=new int[5];
+         int [] arr=new int[5];
         int [] arr1={1,2,3,4,5};
 System.out.println(arr1[3]);
 
     }
 }
+ 
